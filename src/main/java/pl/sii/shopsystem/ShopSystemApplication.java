b869ed31c1840b5260e81fca70ab7c5d1610d0c9
Arsemi337 <arturmilosz@onet.pl>
2023-02-07 08:30:41 +0100

    project replaced wth the one generated by Spring

---
+++ src/main/java/pl/sii/shopsystem/ShopSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sii.shopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSystemApplication.class, args);
+	}
+
+}
