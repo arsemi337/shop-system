e22e321c8c708eacb344e48b8947e14f14d20bc8
Arsemi337 <arturmilosz@onet.pl>
2023-02-28 16:49:16 +0100

    Product microservice created - empty for now

---
+++ product/src/main/java/pl/sii/shopsystem/ShopsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sii.shopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsystemApplication.class, args);
+	}
+
+}
