4d02c0fb1a9991ee3cd220fabb24a22e81c236db
Arsemi337 <arturmilosz@onet.pl>
2023-03-09 14:58:50 +0100

    Startup initial data loading in product service

---
+++ product/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT
+INTO
+  products
+  (id, creation_time, type, title, manufacturer, price, is_deleted)
+VALUES
+  ('7a9c839a-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:23:54', 'Smartphone', 'Pixel 6', 'Google', 479.99, FALSE),
+  ('7a9c87a0-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:24:54', 'Laptop', 'Swift 3', 'Acer', 809.99, FALSE),
+  ('7a9c89e4-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:25:54', 'Drone', 'Mavic Mini 2', 'DJI', 565.99, FALSE),
+  ('7a9c8c64-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:26:54', 'Graphic card', 'GeForce RTX 4090', 'MSI', 2729.99, FALSE),
+  ('7a9c9100-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:27:54', 'Processor', 'i7-10700F', 'Intel', 249.99, FALSE),
+  ('abdb0f08-ac5c-11ed-afa1-0242ac120002', '2004-10-19 10:28:54', 'Monitor', 'P2422H', 'Dell', 199.00, FALSE);
