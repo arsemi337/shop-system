5d9cacb1fbcab6b0a547224c7cf50c9c79fd5d29
Arsemi337 <arturmilosz@onet.pl>
2023-03-17 14:39:06 +0100

    Poison pills handling - when an object, that cannot be deserialized by a consumer, is received from the topic, it is described in logs, but the application doesn't stop working

---
+++ customer/src/main/java/pl/sii/shopsystem/kafka/KafkaCustomerErrorHandler.java
@@ -0,0 +1,23 @@
+package pl.sii.shopsystem.kafka;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.core.log.LogAccessor;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.support.serializer.DeserializationException;
+
+import java.util.Arrays;
+
+public class KafkaCustomerErrorHandler implements CommonErrorHandler {
+
+    @Override
+    public void handleRecord(Exception thrownException, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer, MessageListenerContainer container) {
+        LogAccessor logger = new LogAccessor(KafkaCustomerErrorHandler.class);
+        DeserializationException exception = ReplyingKafkaTemplate.checkDeserialization(record, logger);
+        if (exception != null) {
+            logger.error("The following object couldn't be deserialized: " + Arrays.toString(exception.getData()));
+        }
+    }
+}
--- customer/src/main/java/pl/sii/shopsystem/kafka/ProductKafkaConsumerConfig.java
+++ customer/src/main/java/pl/sii/shopsystem/kafka/ProductKafkaConsumerConfig.java
@@ -3,7 +3,6 @@ package pl.sii.shopsystem.kafka;
 import kafka.dto.ProductDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +11,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -26,17 +26,20 @@ public class ProductKafkaConsumerConfig {
     public Map<String, Object> productConsumerConfig() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        props.put(JsonDeserializer.KEY_DEFAULT_TYPE, "java.lang.String");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "kafka.dto.ProductDto");
+
         return props;
     }
 
     @Bean
     public ConsumerFactory<String, ProductDto> productConsumerFactory() {
         return new DefaultKafkaConsumerFactory<>(
-                productConsumerConfig(),
-                new StringDeserializer(),
-                new JsonDeserializer<>(ProductDto.class));
+                productConsumerConfig());
     }
 
     @Bean
@@ -46,6 +49,7 @@ public class ProductKafkaConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<String, ProductDto> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(productConsumerFactory);
+        factory.setCommonErrorHandler(new KafkaCustomerErrorHandler());
         return factory;
     }
 }
